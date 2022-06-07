c404fda81b6821ad675272913611ade1f5710420
Maksim Rynkov <maximzmei@gmail.com>
2022-06-08 00:15:54 +0400

    fix: fix api link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,8 @@
-
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://2n9tzhp3a5.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://vj89xervo4.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
