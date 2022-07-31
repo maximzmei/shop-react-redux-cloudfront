1f61450ae41bd80010771e37e007da0b30fd2181
Maksim Rynkov <maximzmei@gmail.com>
2022-07-31 15:18:28 +0400

    feat: change cart api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ieochisxhk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://vj89xervo4.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://maximzmei-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
