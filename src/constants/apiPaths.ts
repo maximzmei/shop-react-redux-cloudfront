172c74bf84b160fd8167a16b8d66e7040b4f389a
Maksim Rynkov <maximzmei@gmail.com>
2022-08-11 21:56:02 +0400

    feat: add task 9 features

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'http://maximzmei-bff-api-dev.eu-west-1.elasticbeanstalk.com',
+  order: 'http://maximzmei-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
   import: 'https://ieochisxhk.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://vj89xervo4.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://maximzmei-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  bff: 'http://maximzmei-bff-api-dev.eu-west-1.elasticbeanstalk.com',
+  cart: 'http://maximzmei-bff-api-dev.eu-west-1.elasticbeanstalk.com',
 };
 
 export default API_PATHS;
